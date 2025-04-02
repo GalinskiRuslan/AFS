e764ff844135a58b2bd8f746eb1194822509c6fd
Ruslan Issapov <r_issapov@almaty.gamma.kz>
2025-04-03 01:25:17 +0500

    fix

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -4,17 +4,18 @@ import { observer } from "mobx-react-lite";
 import { appStore } from "@/store/store";
 import { useEffect, useState } from "react";
 import { Company } from "@/Modules/ComaniesPage/Company/Company";
+import { AuthForm } from "@/Modules/layouts/AuthForm/AuthForm";
 
 export default function Companies() {
-    const token =
-      typeof window !== "undefined" ? localStorage.getItem("token") : null;
-    const router = useRouter();
-    useEffect(() => {
-      if (!token) {
-        router.push("/");
-      }
-    }, [token]);
-    if (!token) return null;
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const router = useRouter();
+  useEffect(() => {
+    if (!token) {
+      router.push("/");
+    }
+  }, [token]);
+  if (!token) return <AuthForm />;
   return (
     <div className="content">
       <Company />
