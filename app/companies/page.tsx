4b3c4c0e31b66302bcc450b5c32ba76c83a286da
Ruslan Issapov <r_issapov@almaty.gamma.kz>
2025-04-03 01:21:17 +0500

    fix

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -6,15 +6,15 @@ import { useEffect, useState } from "react";
 import { Company } from "@/Modules/ComaniesPage/Company/Company";
 
 export default function Companies() {
-  //   const token =
-  //     typeof window !== "undefined" ? localStorage.getItem("token") : null;
-  //   const router = useRouter();
-  //   useEffect(() => {
-  //     if (!token) {
-  //       router.push("/");
-  //     }
-  //   }, [token]);
-  //   if (!token) return null;
+    const token =
+      typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    const router = useRouter();
+    useEffect(() => {
+      if (!token) {
+        router.push("/");
+      }
+    }, [token]);
+    if (!token) return null;
   return (
     <div className="content">
       <Company />
